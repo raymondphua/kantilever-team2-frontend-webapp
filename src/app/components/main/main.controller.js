6d10a25e16f8ae4c3f720a0252c3f38230255254
Raymond <info@javaminor2015.com>
2017-01-10 22:10:07 +0100

    sample code of using the directive + service

--- src/app/components/main/main.controller.js
+++ src/app/components/main/main.controller.js
@@ -6,31 +6,32 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, ShoppingCartService, $rootScope, $sessionStorage) {
+  function MainController($timeout) { //ShoppingCartService, $rootScope, $sessionStorage these 3 are needed for shoppingCart
     var vm = this;
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1483973234259;
 
-    vm.click = function() {
-      $rootScope.$emit('cartUpdated', updateCart({ id: $sessionStorage.items.length + 1, name: "test"}, ShoppingCartService.addToCart)); //addToCart(insert product here)
-    };
 
-    vm.clickAgain = function() {
-      $rootScope.$emit('cartUpdated', updateCart({ id: 1, name:'test'}, ShoppingCartService.addToCart));
-    };
+    //Sample code of how to call the cartService for updating the cart
 
-    vm.remove = function() {
-      $rootScope.$emit('cartUpdated', updateCart({ id: 1, name: "test"}, ShoppingCartService.removeFromCart));
-    };
 
-    $rootScope.$on('cartUpdated', function(event) {
-      vm.arraySize = $sessionStorage.items.length;
-    });
 
-    function updateCart(product, shoppingCartFunction) {
-      shoppingCartFunction(product, $sessionStorage.items);
-    }
+    // vm.click = function() {
+    //   $rootScope.$emit('cartUpdated', updateCart({ id: $sessionStorage.items.length + 1, name: "test"}, ShoppingCartService.addToCart)); //addToCart(insert product here)
+    // };
+    //
+    // vm.remove = function() {
+    //   $rootScope.$emit('cartUpdated', updateCart({ id: 1, name: "test"}, ShoppingCartService.removeFromCart));
+    // };
+    //
+    // $rootScope.$on('cartUpdated', function(event) {
+    //   vm.arraySize = $sessionStorage.items.length;
+    // });
+    //
+    // function updateCart(product, shoppingCartFunction) {
+    //   shoppingCartFunction(product, $sessionStorage.items);
+    // }
   }
 })();
