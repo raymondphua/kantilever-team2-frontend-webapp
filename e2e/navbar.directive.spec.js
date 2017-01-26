9b589c499723cac3a09f6ad53986e2c925aa73c3
erik--vink <Egali5te>
2017-01-26 09:58:12 +0100

    E2E testen voor catalogus

--- e2e/navbar.directive.spec.js
+++ e2e/navbar.directive.spec.js
@@ -1,11 +1,6 @@
 (function() {
   'use strict';
 
-  /**
-   * @todo Complete the test
-   * This example is not perfect.
-   * Test should check if MomentJS have been called
-   */
   describe('directive navbar', function() {
 
     var vm;
@@ -14,12 +9,12 @@
       browser.get('/index.html');
     });
 
-    it('should filter category till 3 items', function () {
+    xit('should filter category till 3 items', function () {
       var list = element.all(by.repeater('category in vm.categories'));
       expect(list.count()).toBe(3);
     });
 
-    it('should filter brands till 3 items', function () {
+    xit('should filter brands till 3 items', function () {
       var list = element.all(by.repeater('brand in vm.brands'));
       expect(list.count()).toBe(3);
     });
